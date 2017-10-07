b2852c2c6667e967e77a07615f80ba64f0247472
jokci <jokcik@yandex.ru>
2017-10-07 14:40:24 +0300

    create start form

--- KG_lab_2/Form1.cs
+++ KG_lab_2/Form1.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace KG_lab_2
 {
-    public partial class Form1 : Form
+    public sealed partial class Form1 : Form
     {
+        private void InitializeComponent()
+        {
+            Text = @"Лабораторная работа №2. Графики";
+            Width = 300;
+            Height = 300;
+
+            var panel = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                Padding = new Padding(20),
+                Width = Width - 4 * 20
+            };
+            
+            var label = new Label {Text = @"Функция: y = x", Width = panel.Width};
+            var labelXmax = new Label {Text = @"Введите верхний предел функции:", Width = panel.Width};
+            var labelXmin = new Label {Text = @"Введите нижний предел функции:", Width = panel.Width, Margin = new Padding(0, 10, 0, 0)};
+            var labelStep = new Label {Text = @"Введите шаг сетки:", Width = panel.Width, Margin = new Padding(0, 10, 0, 0)};
+            
+            var textBoxXmax = new TextBox {Text = @"1", Width = panel.Width};
+            var textBoxXmin = new TextBox {Text = @"-1", Width = panel.Width};
+            var textBoxStep = new TextBox {Text = @"100", Width = panel.Width};
+
+            var button = new Button {Text = @"Построить", Margin = new Padding(0, 10, 0, 0)};
+            
+            panel.Controls.Add(label);
+            
+            panel.Controls.Add(labelXmax);
+            panel.Controls.Add(textBoxXmax);
+            
+            panel.Controls.Add(labelXmin);
+            panel.Controls.Add(textBoxXmin);
+         
+            panel.Controls.Add(labelStep);
+            panel.Controls.Add(textBoxStep);
+            
+            panel.Controls.Add(button);
+            Controls.Add(panel);
+        }
+
         public Form1()
         {
             InitializeComponent();
